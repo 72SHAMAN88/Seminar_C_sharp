f303fefc14f8db35febac5b3e39783ea0c3b1e13
72SHAMAN 88 <va.babushkin@mail.ru>
2023-09-20 01:40:46 +0500

    add Sem1 tasks project

---
+++ Seminars/Sem1/Program.cs
@@ -0,0 +1,61 @@
+﻿//System.Console.WriteLine("Input number: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//System.Console.WriteLine($"Your number -> {num} ");
+
+// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+
+//System.Console.Write("Input number: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//System.Console.WriteLine($"Quad of {num} -> {num*num}");
+
+// Напишите программу, которая на вход принимает трёхзначное число и на выходе показывает **псоледнюю** цифру этого числа.
+
+//   456 -> 6   456 % 10 => 6
+
+//   782 -> 2
+
+//   918 -> 8
+
+//System.Console.Write("Input three-digit number: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//if(num >= 100 && num < 1000)
+//{
+//    System.Console.WriteLine($"Last digit of {num} -> {num%10}");
+//}
+//else
+//{
+//    System.Console.WriteLine("You input not three-digit number");
+//}
+
+// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+
+// 2 -> " -2, -1, 0, 1, 2"
+
+//System.Console.Write("Input number: ");
+//int n = Convert.ToInt32(Console.ReadLine());
+//int count = -n;
+//while (count <=n) 
+//    System.Console.Write(count++);
+
+//  Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+
+// a = 25; b = 5 -> да
+// a = 2 b = 10 -> нет
+// a = 9; b = -3 -> да
+// a = -3 b = 9 -> нет
+
+//System.Console.Write("Your first number: ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//System.Console.Write("Your second number: ");
+//int b = Convert.ToInt32(Console.ReadLine());
+//if(a == b*b)
+//{
+//    System.Console.Write($"{a} is quad {b}");
+//}
+//else
+//{
+//    System.Console.Write($"{a} is not quad {b}");
+//}
