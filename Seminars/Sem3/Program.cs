9a8e754eaa42eed33b226a73036c4975f82587f2
72SHAMAN 88 <va.babushkin@mail.ru>
2023-10-01 19:35:48 +0500

    added Sem3, Sem4

---
+++ Seminars/Sem3/Program.cs
@@ -0,0 +1,87 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+// int FindQaurter(int x, int y)
+// {
+//     if (x > 0 && y > 0) return 1;
+//     if (x < 0 && y > 0) return 2;
+//     if (x < 0 && y < 0) return 3;
+//     if (x > 0 && y < 0) return 4;
+//     return 0;
+// }
+
+// System.Console.Write("Input x: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Input y: ");
+// int y = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(FindQaurter(x, y));
+
+// Напишите программу которая на принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до (N).
+// 5 -> 1, 4, 9, 16, 25
+// 2 -> 1, 4
+
+// void QuadTable(int n)
+// {
+//     int current = 1;
+//     System.Console.Write(n + " -> ");
+//     while (current < n)
+//     {
+//         System.Console.Write(current * current + ", ");
+//         current++;
+//     }
+//     System.Console.Write(current * current);
+// }
+
+// System.Console.Write("Input n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// QuadTable(n);
+
+// Напишите программу которая, по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// void Coord_quarter(int qaurter)
+// {
+//     if (qaurter == 1)
+//     {
+//         System.Console.WriteLine("x > 0 и y > 0");
+//     }
+//     else if (qaurter == 2)
+//     {
+//         System.Console.WriteLine("x < 0 и y > 0");
+//     }
+//     else if (qaurter == 3)
+//     {
+//         System.Console.WriteLine("x < 0 и y < 0");
+//     }
+//     else if (qaurter == 4)
+//     {
+//         System.Console.WriteLine("x > 0 и y < 0");
+//     }
+//     else
+//     {
+//         System.Console.WriteLine("Error!");
+//     }
+// }
+
+// System.Console.Write("Input qaurter: ");
+// int q = Convert.ToInt32(Console.ReadLine());
+// Coord_quarter(q);
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+// А(3, 6) В(2, 1) -> 5,09
+// А(7, -5) В(1, -1) -> 7,21
+
+double Distance(double xa, double xb, double ya, double yb)
+{
+    return Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2));
+}
+
+System.Console.WriteLine("Введите знанчение координат: ");
+System.Console.Write("Ведите координату X точки A: ");
+double xa = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Ведите координату X точки B: ");
+double xb = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Ведите координату Y точки A: ");
+double ya = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Ведите координату Y точки B: ");
+double yb = Convert.ToDouble(Console.ReadLine());
+System.Console.WriteLine($"A({xa}, {ya}) B({xb}, {yb}) -> {Math.Round(Distance(xa, xb, ya, yb), 2)}");
---
+++ Seminars/Sem4/Program.cs
@@ -0,0 +1,123 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+// Вариант 1
+
+// int DigitCount(int num)
+// {
+//     int count = 1;
+//     for(int i = 0; num > 10; i++)
+//     {
+//         num /= 10;
+//         count++;
+//     }
+//     return count;
+// }
+
+// System.Console.Write("Input num: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(DigitCount(num));
+
+// Вариант 2
+
+// int DigitCount(int num)
+// {
+//     int count = 0;
+//     for(int i = 1; num > 0; i++)
+//     {
+//         num /= 10;
+//         count = i;
+//     }
+//     return count;
+// }
+
+// System.Console.Write("Input num: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(DigitCount(num));
+
+// Вариант 3 с отрицательными числами
+
+// int DigitCount(int num)
+// {
+//     int count = 0;
+//     for(int i = 1; Math.Abs(num) > 0; i++)
+//     {
+//         num /= 10;
+//         count = i;
+//     }
+//     return count;
+// }
+
+// System.Console.Write("Input num: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(DigitCount(num));
+
+// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+// Вариант для массива любого размера
+
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         System.Console.Write(array[i] + " ");
+//     }
+// }
+
+// System.Console.Write("Input size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Input min value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Input max value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+// PrintArray(CreateRandomArray(size, min, max));
+
+// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+// int FindSum(int a)
+// {
+//     int sum = 0;
+//     for (int i = 0; i <= a; i++)
+//     {
+//         sum += i;
+//     }
+//     return sum;
+// }
+
+// System.Console.Write("Input A: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write($"{a} -> {FindSum(a)}");
+
+// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4  -> 24
+// 5  -> 120
+
+int FindFactorial(int n)
+{
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+
+System.Console.Write("Input N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.Write($"{n} -> {FindFactorial(n)}");
