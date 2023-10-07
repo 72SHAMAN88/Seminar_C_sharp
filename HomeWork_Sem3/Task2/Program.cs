8db6c0f223302dc790892f75f01e48429d364cd3
72SHAMAN 88 <va.babushkin@mail.ru>
2023-10-07 20:37:16 +0500

    added HomeWork_Sem3

---
+++ HomeWork_Sem3/Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Answer
+{
+    private static double Length(int[] pointA, int[] pointB)
+    {
+      // Введите свое решение ниже
+      int A = pointB[0] - pointA[0];
+      int B = pointB[1] - pointA[1];
+      int C = pointB[2] - pointA[2];
+      double length = Math.Sqrt(A * A + B * B + C * C);
+      return length;
+
+    }
+
+  // Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args) {
+        int x1, x2, x3, y1, y2, y3;
+
+        if (args.Length >= 6) {
+            x1 = int.Parse(args[0]);
+            x2 = int.Parse(args[1]);
+            x3 = int.Parse(args[2]);
+            y1 = int.Parse(args[3]);
+            y2 = int.Parse(args[4]);
+            y3 = int.Parse(args[5]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            x1 = 7;
+            x2 = -5;
+            x3 = 0;
+            y1 = 1;
+            y2 = -1;
+            y3 = 9;
+        }
+
+        // Не удаляйте строки ниже
+        double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
+        Console.WriteLine($"{result:F2}");
+    }
+}
